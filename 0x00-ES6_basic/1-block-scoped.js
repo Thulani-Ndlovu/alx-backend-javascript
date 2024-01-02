5bdde0afcf2c2a22cc582c87e48fc6de9cd720fc
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-02 19:27:34 +0200

    Task 1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      return [task, task2]
-    }
-  
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
     return [task, task2];
   }
-  
+
+  return [task, task2];
+}
