e0b7f80d78c4839a99a0f726239b18363957b222
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-03 23:40:05 +0200

    Task 1. Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  const response = {
-    status: '200',
-    body: 'Success',
-  };
   const promise = new Promise(((resolve, reject) => {
-    if (success) {
+    const response = {
+      status: '200',
+      body: 'Success',
+    };
+    if (success === true) {
       resolve(response);
     } else {
       reject(Error('The fake API is not working currently'));
