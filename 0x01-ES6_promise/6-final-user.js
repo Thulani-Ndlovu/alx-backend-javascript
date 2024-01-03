b5ecfd213d0b6a2210af485117b2dec5bb96f65f
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-04 00:58:26 +0200

    Task 6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => {
+    const array = [];
+    for (const idx of data) {
+      array.push({ status: idx.status, value: idx.value || idx.status });
+    }
+    return array;
+  });
+}
