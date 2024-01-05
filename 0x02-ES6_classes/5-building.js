d6061d15ea5ee38cae98182648cc742f3c63db21
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-06 00:45:59 +0200

    Task 5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqrt must be a number');
-    }
     if (this.constructor !== Building && typeof
     this.evacuationWarningMessage !== 'function') {
       throw new TypeError('Class extending Building must override evacuationWarningMessage');
