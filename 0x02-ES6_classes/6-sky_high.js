5de101db87ce87f0ec19f2084b1772bdf73c2e33
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-06 01:09:53 +0200

    Task 6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,14 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() { return this._floors; }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this._floors} floors`);
+  }
+}
