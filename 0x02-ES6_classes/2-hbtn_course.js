6c1bb697bb6467e7b63ef24b9ca6b92a1db0950b
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-05 18:21:01 +0200

    Task 2. A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be an object');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() { return this._name; }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() { return this._length; }
+
+  set students(students) {
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be an object');
+    }
+    this._students = students;
+  }
+
+  get students() { return this._students; }
+}
