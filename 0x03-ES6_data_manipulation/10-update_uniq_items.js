83a313e27de6d28b6ae94b34cd1590c20de23072
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-08 23:30:07 +0200

    Task 10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((val, key) => {
+    if (val === 1) {
+      map.set(key, 100);
+    }
+  });
+}
