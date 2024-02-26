8737fe15dee9addc21a2574b474d095298c040cf
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-26 19:15:23 +0200

    Task 1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on("readable", () => {
-    const name = process.stdin.read();
-
-    if (name) {
-        process.stdout.write(`Your name is: ${name}`);
-    }
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on("end", () => {
-    process.stdout.write("This important software is now closing\n");
+  process.stdout.write("This important software is now closing\n");
 });
