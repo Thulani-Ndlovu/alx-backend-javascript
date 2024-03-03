8073e1a3b4e498d9ed8af387afa6ff35b05d4b5a
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-03-03 12:39:59 +0200

    Task 0. Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Your Test Suite', () => {
+  it('should pass when true is true', () => {
+    assert.strictEqual(true, true);
+  });
+
+  // Add more tests as needed
+});
