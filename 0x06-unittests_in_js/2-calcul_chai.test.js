ed7096794dbbb5dd30073604379487d302d3bb5a
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-03-05 11:16:39 +0200

    Task 2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
-const expect = require('chai');
-const calculateNumber = require('./2-calcul');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   describe('SUM', () => {
