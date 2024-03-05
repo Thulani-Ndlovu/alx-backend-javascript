7d71f845e15d60cfa04f9a9ef6e87693bef1631b
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-03-05 10:33:07 +0200

    Task 1. Combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ const calculateNumber = function(type, a, b) {
   switch (type) {
     case 'SUM': return Math.round(a) + Math.round(b);
     case 'SUBTRACT': return Math.round(a) - Math.round(b);
-    case 'DIVIDE': return (Math.round(b) !== 0) ? Math.round(a) / Math.round(b) : "Error";
+    case 'DIVIDE': return (b !== 0) ? (a) / (b) : "Error";
     default: "Invalid type";
   }
 
