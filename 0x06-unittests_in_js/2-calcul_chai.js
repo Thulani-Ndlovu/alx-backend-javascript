714194ceca11b0ef793f9384498a8a8530bc0831
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-03-05 11:10:32 +0200

    Task 2. Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,11 @@
+const calculateNumber = function(type, a, b) {
+    switch (type) {
+      case 'SUM': return Math.round(a) + Math.round(b);
+      case 'SUBTRACT': return Math.round(a) - Math.round(b);
+      case 'DIVIDE': return (b !== 0) ? (a) / (b) : "Error";
+      default: "Invalid type";
+    }
+  
+  };
+  
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,62 +1,62 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const expect = require('chai');
+const calculateNumber = require('./2-calcul');
 
 describe('calculateNumber', () => {
   describe('SUM', () => {
     it('should return whole numbers from floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUM", 2.0, 2.0), 4);
+      expect(calculateNumber("SUM", 2.0, 2.0)).to.equal(4);
     });
   
     it('should round down the b argument floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUM", 2.0, 3.3), 5);
+      expect(calculateNumber("SUM", 2.0, 3.3)).to.equal(5);
     });
   
     it('should round down the a argument floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUM", 1.4, 2.0), 3);
+      expect(calculateNumber("SUM", 1.4, 2.0)).to.equal(3);
     });
   
     it('should return a whole number when only a\ is a float', () => {
-      assert.strictEqual(calculateNumber("SUM", 5.2, 2), 7);
+      expect(calculateNumber("SUM", 5.2, 2)).to.equal(7);
     });
   
     it('should return a whole number when only a\ is a float', () => {
-      assert.strictEqual(calculateNumber("SUM", 5, 4.5), 10);
+      expect(calculateNumber("SUM", 5, 4.5)).to.equal(10);
     });
   
     it('it should round up a\ argument when its a float', () => {
-      assert.strictEqual(calculateNumber("SUM", 5.5, 4), 10);
+      expect(calculateNumber("SUM", 5.5, 4)).to.equal(10);
     });
   
     it('it should round up b\ argument when its a float', () => {
-      assert.strictEqual(calculateNumber("SUM", 4, 5.5), 10);
+      expect(calculateNumber("SUM", 4, 5.5)).to.equal(10);
     });
   
     it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
-      assert.strictEqual(calculateNumber("SUM", 2.499999, 3.499999), 5);
+      expect(calculateNumber("SUM", 2.499999, 3.499999)).to.equal(5);
     });
   
     it('should handle both negative inputs', () => {
-      assert.strictEqual(calculateNumber("SUM", -10, -1), -11);
+      expect(calculateNumber("SUM", -10, -1)).to.equal(-11);
     });
     
     it('should handle a negative a\ argument input', () => {
-      assert.strictEqual(calculateNumber("SUM", -11, 1), -10);
+      expect(calculateNumber("SUM", -11, 1)).to.equal(-10);
     });
   
     it('should handle a negative b\ argument input', () => {
-      assert.strictEqual(calculateNumber("SUM", 11, -1), 10);
+      expect(calculateNumber("SUM", 11, -1)).to.equal(10);
     });
   
     it('should handle a case where both inputs are zero', () => {
-      assert.strictEqual(calculateNumber("SUM", 0, 0), 0);
+      expect(calculateNumber("SUM", 0, 0)).to.equal(0);
     });
   
     it('should handle one large floating number and a small one', () => {
-      assert.strictEqual(calculateNumber("SUM", 999999999.5, 0.5), 1000000001);
+      expect(calculateNumber("SUM", 999999999.5, 0.5)).to.equal(1000000001);
     });
   
     it('should handle both large floating points', () => {
-      assert.strictEqual(calculateNumber("SUM", 999999999.5, 999999999.5), 2000000000);
+      expect(calculateNumber("SUM", 999999999.5, 999999999.5)).to.equal(2000000000);
     });
   
   });
@@ -65,59 +65,59 @@ describe('calculateNumber', () => {
   
   
     it('should return whole numbers from floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 2.0, 2.0), 0);
+      expect(calculateNumber("SUBTRACT", 2.0, 2.0)).to.equal(0);
     });
   
     it('should round down the b\ argument floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 2.0, 3.3), -1);
+      expect(calculateNumber("SUBTRACT", 2.0, 3.3)).to.equal(-1);
     });
   
     it('should round down the a\ argument floating point inputs', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 2.0), -1);
+      expect(calculateNumber("SUBTRACT", 1.4, 2.0)).to.equal(-1);
     });
   
     it('should return a whole number when only a\ is a float', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 5.2, 2), 3);
+      expect(calculateNumber("SUBTRACT", 5.2, 2)).to.equal(3);
     });
   
     it('should return a whole number when only a\ is a float', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 5, 4.5), 0);
+      expect(calculateNumber("SUBTRACT", 5, 4.5)).to.equal(0);
     });
   
     it('it should round up a\ argument when its a float', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 5.5, 4), 2);
+      expect(calculateNumber("SUBTRACT", 5.5, 4)).to.equal(2);
     });
   
     it('it should round up b\ argument when its a float', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 4, 5.5), -2);
+      expect(calculateNumber("SUBTRACT", 4, 5.5)).to.equal(-2);
     });
   
     it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 2.499999, 3.499999), -1);
+      expect(calculateNumber("SUBTRACT", 2.499999, 3.499999)).to.equal(-1);
     });
   
     it('should handle both negative inputs', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", -10, -1), -9);
+      expect(calculateNumber("SUBTRACT", -10, -1)).to.equal(-9);
     });
     
     it('should handle a negative a\ argument input', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", -11, 1), -12);
+      expect(calculateNumber("SUBTRACT", -11, 1)).to.equal(-12);
     });
   
     it('should handle a negative b\ argument input', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 11, -1), 12);
+      expect(calculateNumber("SUBTRACT", 11, -1)).to.equal(12);
     });
   
     it('should handle a case where both inputs are zero', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 0, 0), 0);
+      expect(calculateNumber("SUBTRACT", 0, 0)).to.equal(0);
     });
   
     it('should handle one large floating number and a small one', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 999999999.5, 0.5), 999999999);
+      expect(calculateNumber("SUBTRACT", 999999999.5, 0.5)).to.equal(999999999);
     });
   
     it('should handle both large floating points', () => {
-      assert.strictEqual(calculateNumber("SUBTRACT", 999999999.5, 999999999.5), 0);
+      expect(calculateNumber("SUBTRACT", 999999999.5, 999999999.5)).to.equal(0);
     });
   
   });
@@ -125,27 +125,27 @@ describe('calculateNumber', () => {
   describe('DIVIDE', () => {
   
     it('should handle a denominator of zero', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", 5, 0), "Error");
+      expect(calculateNumber("DIVIDE", 5, 0)).to.equal("Error");
     });
   
     it('it should return a positive result when a\ & b\ are negative', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", -5, -5), 1);
+      expect(calculateNumber("DIVIDE", -5, -5)).to.equal(1);
     });
   
     it('it should return a positive result when ONLY a\ negative', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", -5, 5), -1);
+      expect(calculateNumber("DIVIDE", -5, 5)).to.equal(-1);
     });
   
     it('it should return a negative result when ONLY b\ is negative', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", 5, -5), -1);
+      expect(calculateNumber("DIVIDE", 5, -5)).to.equal(-1);
     });
   
     it('it should return a positive result when BOTH a\ and b\ are positive', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", 5, 5), 1);
+      expect(calculateNumber("DIVIDE", 5, 5)).to.equal(1);
     });
   
     it('it should return a positive floating point when both a\ and b\ are positive', () => {
-      assert.strictEqual(calculateNumber("DIVIDE", 5.5, 2.5), 2.2)
+      expect(calculateNumber("DIVIDE", 5.5, 2.5).to.equal(2.2))
     });
   });
 });
